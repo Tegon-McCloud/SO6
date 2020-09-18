09a38fafeccc252697a3640b93d4d1faae865993
Beckudrup <de3gutter@gmail.com>
2020-09-18 17:42:23 +0200

    #18 upgrades

--- src/so6/ui/UpgradeMenu.java
+++ src/so6/ui/UpgradeMenu.java
@@ -1,6 +1,7 @@
 package so6.ui;
 
 import processing.core.*;
+import so6.Window;
 import so6.base.Tower;
 
 import javax.imageio.ImageIO;
@@ -12,46 +13,52 @@ public class UpgradeMenu {
     private PImage img;
     private boolean showUMenu = false;
 
-   public UpgradeMenu() throws IOException {
-       img = new PImage(ImageIO.read(new File("./resources/Upgrade menu/Upgrade menu.png")));
+    public UpgradeMenu() throws IOException {
+        img = new PImage(ImageIO.read(new File("./resources/Upgrade menu/Upgrade menu.png")));
 
     }
-    public void draw(PGraphics g){
 
-       if(showUMenu){
+    public void draw(PGraphics g) {
+
+        if (showUMenu) {
             g.imageMode(PConstants.CENTER);
-            g.image(img,pos.x,pos.y);
+            g.image(img, pos.x, pos.y);
         }
 
     }
-    public void toggleUM(){
+
+    public void toggleUM() {
         showUMenu = !showUMenu;
 
     }
-    public void getTowerPos(Tower tower){
+
+    public void getTowerPos(Tower tower) {
         pos = tower.getPosition();
 
     }
-/*
-    public void rangeUpgrade(){
-        if (mouseX >= pos.x + 3 &&         // right of the left edge AND
-                mouseX <= pos.x + 11 &&    // left of the right edge AND
-                mouseY >= pos.y + 3 &&         // below the top AND
-                mouseY <= pos.y + 11) {    // above the bottom
-        }
 
-    }
-    private void speedUpgrade(){
-        if (mouseX >= pos.x - 3 &&         // right of the left edge AND
-                mouseX <= pos.x - 11 &&    // left of the right edge AND
-                mouseY >= pos.y - 3 &&         // below the top AND
-                mouseY <= pos.y - 11) {    // above the bottom
-        }
+    public void rangeUpgrade() {
+        int mouseX = Window.getWnd().mouseX;
+        int mouseY = Window.getWnd().mouseY;
 
+        if (mouseX >= pos.x + 3 &&
+                mouseX <= pos.x + 11 &&
+                mouseY >= pos.y + 3 &&
+                mouseY <= pos.y + 11) {
+            System.out.println("heya");
+        }
     }
 
- */
-
+    private void speedUpgrade() {
+        int mouseX = Window.getWnd().mouseX;
+        int mouseY = Window.getWnd().mouseY;
+        if (mouseX >= pos.x - 3 &&
+                mouseX <= pos.x - 11 &&
+                mouseY >= pos.y - 3 &&
+                mouseY <= pos.y - 11) {
+            System.out.println("hoopla");
+        }
+    }
 }
 
 
